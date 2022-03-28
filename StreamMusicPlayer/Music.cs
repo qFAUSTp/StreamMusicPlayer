f423b57ea54658f3d86ca79513220b849a1ea138
Alex K <19397049+qFAUSTp@users.noreply.github.com>
2022-03-29 00:15:05 +0300

    Project upload

---
+++ StreamMusicPlayer/Music.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace StreamMusicPlayer
+{
+    public enum States { NoMusic, Playing, Paused }
+    public class Music
+    {
+
+        public List<string> PlayList { get; private set; }
+        public int CurrentPlaying { get; set; }
+        public int LastSong { get; private set; }
+
+        public States MusicState { get; set; }
+
+        private MediaPlayer songStream;
+
+        public SongAlert mSongAlert { get; set; }
+
+        public Music(string path, MainWindow mainWindow)
+        {
+            MusicState = States.NoMusic;
+            LoadNewPlaylist(path, mainWindow);
+        }
+
+        public void LoadNewPlaylist(string path, MainWindow mainWindow)
+        {
+            PlayList = new List<string>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string textLine = null;
+                while (!string.IsNullOrEmpty(textLine = sr.ReadLine()))
+                    if (textLine.Contains(@"..\..\..\..\"))
+                    {
+                        int i = textLine.IndexOf(".mp3");
+                        PlayList.Add(textLine.Substring(36, i - 36 + 4));
+                    }
+            }
+
+            CurrentPlaying = 0;
+            LastSong = PlayList.Count() - 1;
+
+            songStream = new MediaPlayer();
+            songStream.MediaEnded += (snd, EventArgs) => {
+                CurrentPlaying++;
+                MusicState = States.NoMusic;
+                PlayMusic();
+                mainWindow.UpdateCover();
+                mSongAlert.RedrawWindow();
+            };
+        }
+
+        public void PlayMusic()
+        {
+            //Console.WriteLine(musicState);
+            switch (MusicState)
+            {
+                //Start playing new playlist.
+                case States.NoMusic:
+                    MusicState = States.Playing;
+                    Console.WriteLine(MusicState);
+                    PlaySong();
+                    break;
+                //Pause currently playing music.
+                case States.Playing:
+                    MusicState = States.Paused;
+                    Console.WriteLine(MusicState);
+                    songStream.Pause();
+                    break;
+                //Continue playing currently stopped music.
+                case States.Paused:
+                    MusicState = States.Playing;
+                    Console.WriteLine(MusicState);
+                    songStream.Play();
+                    break;
+            }
+        }
+
+        private void PlaySong()
+        {
+            mSongAlert.RedrawWindow();
+            songStream.Stop();
+            if (CurrentPlaying >= 0 && CurrentPlaying <= LastSong)
+            {
+                songStream.Open(new Uri(@"C:\" + PlayList[CurrentPlaying]));
+                songStream.Play();
+            }
+            else return;
+        }
+
+        public string GetCurrentSong()
+        {
+            return PlayList[CurrentPlaying];
+        }
+
+    }
+}
---
+++ StreamMusicPlayer/SongAlert.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StreamMusicPlayer
+{
+    public partial class SongAlert : Window
+    {
+        MainWindow mainWindow;
+
+        private ThicknessAnimation borderAnimationAppear;
+        private DoubleAnimation coverAnimationAppear;
+        private DoubleAnimation layerAnimationAppear;
+        private DoubleAnimation layerAnimationDisappear;
+
+        private bool beginAnimationIsEnded;
+        public bool BindingState { get; set; } 
+
+        public SongAlert(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+
+            this.WindowStyle = WindowStyle.None;
+            this.AllowsTransparency = true;
+            this.Background = Brushes.Transparent;
+
+            //https://www.youtube.com/watch?v=TAJ7ZXdfCv8
+            //https://professorweb.ru/my/WPF/binding_and_styles_WPF/level11/11_8.php
+
+            SetAnimation();
+            Show();
+            //RedrawWindow();
+        }
+        private void UpdateCoverImg()
+        {
+            //  Update cover image in thi main window;
+            using (var file = TagLib.File.Create(@"C:\" + mainWindow.music.GetCurrentSong()))
+            {
+                TagLib.IPicture pic = file.Tag.Pictures[0];
+                MemoryStream ms = new MemoryStream(pic.Data.Data);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.StreamSource = ms;
+                bitmap.EndInit();
+
+                Image.Source = bitmap;
+
+                tagGroupLbl.Content = null;
+               // StringBuilder performers = new StringBuilder();
+                foreach(string performer in file.Tag.Performers)
+                    tagGroupLbl.Content += performer;
+                tagAlbomLbl.Content = file.Tag.Album;
+                tagNameLbl.Content = file.Tag.Title;
+                
+            }
+
+        }
+
+        private void SetAnimation()
+        {   
+            // Set appear animation
+            borderAnimationAppear = new ThicknessAnimation();
+            borderAnimationAppear.From = new Thickness(-20, 25, 20, 25);
+            borderAnimationAppear.To = new Thickness(-40, 25, 40, 25);
+            borderAnimationAppear.Duration = TimeSpan.FromSeconds(2);
+
+            coverAnimationAppear = new DoubleAnimation();
+            coverAnimationAppear.From = 160;
+            coverAnimationAppear.To = 180;
+            coverAnimationAppear.Duration = TimeSpan.FromSeconds(2);
+
+            layerAnimationAppear = new DoubleAnimation();
+            layerAnimationAppear.From = 0;
+            layerAnimationAppear.To = 1;
+            layerAnimationAppear.Duration = TimeSpan.FromSeconds(1);
+            
+            coverAnimationAppear.Completed += async (s, e) =>
+            {
+                if (!beginAnimationIsEnded)
+                {
+                    beginAnimationIsEnded = true;
+                    Console.WriteLine("Animation completed.");
+                    Console.WriteLine("{Thread.Sleep(2000);} - Start.");
+                    await Task.Delay(2000);
+                    Console.WriteLine("{Thread.Sleep(2000);} - Done.");
+                    AnimationDisappear();
+                }
+            };
+
+            // Set disappear animation
+            layerAnimationDisappear = new DoubleAnimation();
+            layerAnimationDisappear.From = 1;
+            layerAnimationDisappear.To = 0;
+            layerAnimationDisappear.Duration = TimeSpan.FromSeconds(1);
+        } 
+
+        private void AnimationAppear()
+        {
+            Show();
+            beginAnimationIsEnded = false;
+            songAlert.BeginAnimation(OpacityProperty, layerAnimationAppear);
+            Image.BeginAnimation(WidthProperty, coverAnimationAppear);
+            Image.BeginAnimation(HeightProperty, coverAnimationAppear);
+            BorderData.BeginAnimation(MarginProperty, borderAnimationAppear);   
+        }
+
+        public void AnimationDisappear()
+        {
+            songAlert.BeginAnimation(OpacityProperty, layerAnimationDisappear);
+            //Image.BeginAnimation(WidthProperty, coverAnimationDisappear);
+            //Image.BeginAnimation(HeightProperty, coverAnimationDisappear);
+            //BorderData.BeginAnimation(MarginProperty, borderAnimationDisappear);
+        }
+    
+        public void RedrawWindow()
+        {
+            UpdateCoverImg();
+            AnimationAppear();     
+        }
+
+        private void Mouse_Click_DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (BindingState == false)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
